397dca1a2a1660ecb139741af74b71f916076c53
santocaridi99 <santocaridi99@gmail.com>
2022-03-03 16:42:41 +0100

    aggiunto navbar all'home

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,21 @@
-{{-- qui andrà la navbar uguale per tutte le pagine del sito --}}
-<div class="navbar">
-    <h1>qui va la mia navbar</h1>
+{{-- qui andrà la header uguale per tutte le pagine del sito --}}
+<div class="additional ">
+
+</div>
+<div class="header d-flex justify-content-around align-items-center p-4 mt-2">
+    <div class="logo">
+        <img src="{{asset('img/dc-logo.png')}}" class="img-fluid" alt="">
+    </div>
+    <ul class="d-flex m-0">
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+    </ul>
+    <input type="text" placeholder="search">
 </div>
